8834e1ce90c399e53404f780ceb7919de17ef33a
Pedro Rios <PRios731@gmail.com>
2024-01-29 18:30:58 -0300

    Spotify clone utilizando react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Sidebar from './components/Sidebar/Sidebar';
+import Main from './components/Main/Main';
+
+function App() {
+  return (
+    <div>
+      <Sidebar></Sidebar>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </div>
+    
+  );
+}
+
+export default App;
